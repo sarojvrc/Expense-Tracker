e2989a70018d3d34806b365a042eb3c54b8b865c
sarojvrc <saroj.vrc@gmail.com>
2021-11-28 15:22:17 +0530

    new transaction updated

--- src/Components/NewTransaction.js
+++ src/Components/NewTransaction.js
@@ -1,5 +1,5 @@
 import { Box, TextField, Typography, makeStyles, Button } from "@material-ui/core";
-import React from "react";
+import React, {useState} from "react";
 
 const useStyle = makeStyles({
     container:{
@@ -15,14 +15,26 @@ const useStyle = makeStyles({
     }
 })
 
-function NewTransaction() {
+function NewTransaction({addTransaction}) {
     const classes = useStyle();
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState();
+
+    const newTransaction = () => {
+        const transaction = {
+            id: Math.floor(Math.random() * 10000),
+            text:text,
+            amount: +amount
+        }
+        addTransaction(transaction);
+    }
+
   return (
     <Box className={classes.container}>
       <Typography variant="h5">New Transaction</Typography>
-      <TextField label="Enter Expense" />
-      <TextField label="Total Amount" />
-      <Button className={classes.button} variant="contained"> Add New Transaction </Button>
+      <TextField label="Enter Expense" onChange={(e) => setText(e.target.value)} />
+      <TextField label="Total Amount" onChange={(e) => setAmount(e.target.value)} />
+      <Button className={classes.button} variant="contained" onClick={newTransaction}> Add New Transaction </Button>
     </Box>
   );
 }
