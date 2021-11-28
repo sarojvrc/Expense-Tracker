e452f1dc2295382266928e7af4d9a78a69387f3c
sarojvrc <saroj.vrc@gmail.com>
2021-11-28 15:31:19 +0530

    transaction updated

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,29 +1,38 @@
-import { Box, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core'
-import React from 'react'
-import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  Box,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+} from "@material-ui/core";
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const useStyle = makeStyles({
-    list:{
-        marginTop: 10,
-        border: '1px solid #f6f6f6'
-    }
-})
+  list: {
+    marginTop: 10,
+    border: "1px solid #f6f6f6",
+  },
+});
 
-function Transactions({transaction, deleteTransaction}) {
-    const classes = useStyle();
-    const sign = transaction.amount >= 0 ? '₹' : '-'; 
-    const color = transaction.amount >= 0 ? "Green" : "Red";
-    const amount = sign + Math.abs(transaction.amount);
-    
-    return (
-        <ListItem className={classes.list} style={{background:`${color}`, color:"White"}}>
-           <ListItemIcon>
-               <DeleteIcon onClick={() => deleteTransaction(transaction.id)} />
-           </ListItemIcon>
-            <ListItemText primary={transaction.text} />
-            <ListItemText primary={amount} />
-        </ListItem>
-    )
+function Transactions({ transaction, deleteTransaction }) {
+  const classes = useStyle();
+  const sign = transaction.amount >= 0 ? "₹" : "-";
+  const color = transaction.amount >= 0 ? "Green" : "Red";
+  const amount = sign + Math.abs(transaction.amount);
+
+  return (
+    <ListItem
+      className={classes.list}
+      style={{ background: `${color}`, color: "White" }}
+    >
+      <ListItemIcon>
+        <DeleteIcon onClick={() => deleteTransaction(transaction.id)} />
+      </ListItemIcon>
+      <ListItemText primary={transaction.text} />
+      <ListItemText primary={amount} />
+    </ListItem>
+  );
 }
 
-export default Transactions
+export default Transactions;
