372248097ecc6c57da82435f57dd5321216452b3
sarojvrc <saroj.vrc@gmail.com>
2021-11-28 15:22:07 +0530

    expense card updated

--- src/Components/ExpenseCard.js
+++ src/Components/ExpenseCard.js
@@ -11,7 +11,7 @@ const useStyle = makeStyles({
   container: {
     display: "flex",
     "& > *": {
-      padding: 10,
+      padding: 8,
       flex: 1,
     },
   },
@@ -23,21 +23,24 @@ const useStyle = makeStyles({
   },
 });
 
-function ExpenseCard() {
+function ExpenseCard({transactions}) {
   const classes = useStyle();
+  const amount = transactions.map(transaction => transaction.amount);
+  const income = amount.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const expense = (amount.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
   return (
     <Box className={classes.container}>
       <Card>
         <CardContent>
           <Typography>Income</Typography>
-          <Typography className={classes.income}>Rs 1000</Typography>
+          <Typography className={classes.income}>₹{income}</Typography>
         </CardContent>
       </Card>
 
       <Card>
         <CardContent>
-          <Typography>Income</Typography>
-          <Typography className={classes.expense}>Rs 1000</Typography>
+          <Typography>Expense</Typography>
+          <Typography className={classes.expense}>₹{expense}</Typography>
         </CardContent>
       </Card>
     </Box>
